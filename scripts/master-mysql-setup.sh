79dc6a51333891e07c4c9fd201d82af2c055ced8
fenixvd <mrfenixvd@yandex.ru>
2024-06-23 20:28:53 +0300

    update dpkg script

--- scripts/master-mysql-setup.sh
+++ scripts/master-mysql-setup.sh
@@ -46,8 +46,7 @@ ZABBIX_AGENT_CONF="/etc/zabbix/zabbix_agent2.conf"
 # Установка необходимых зависимостей
 sudo apt update -y
 sudo apt install -y wget gnupg libldap-common libssl-dev
-curl -O http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.22_amd64.deb
-sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2.22_amd64.deb
+sudo dpkg -i dotfiles/libssl1.1_1.1.1f-1ubuntu2.22_amd64.deb
 
 # Добавление репозитория Zabbix
 wget https://repo.zabbix.com/zabbix/7.0/ubuntu/pool/main/z/zabbix/zabbix-agent2_7.0.0-1%2Bubuntu24.04_amd64.deb
